0710d61cc8e250726957bb49e065397af1318e39
Timothy025 <timothy025@gmail.com>
2025-06-09 22:03:25 +0530

    chore: update build configuration for Font Awesome compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,12 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    transpilePackages: [
+        '@fortawesome/fontawesome-svg-core',
+        '@fortawesome/free-solid-svg-icons',
+        '@fortawesome/free-regular-svg-icons',
+        '@fortawesome/free-brands-svg-icons',
+        '@fortawesome/react-fontawesome'
+    ]
 };
 export default nextConfig;
